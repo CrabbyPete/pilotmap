b10ee6ea6cc8e83fd94f68040180e29ae0849870
Pete Douma <86552+CrabbyPete@users.noreply.github.com>
2024-11-26 15:51:50 -0500

    run test

--- src/oled_test.py
+++ src/oled_test.py
@@ -23,6 +23,11 @@ SPI_DEVICE = 0
 # SPI_DEVICE = 0
 
 # 128x32 display with hardware I2C:
+from Adafruit_GPIO  import I2C
+tca = I2C.get_i2c_device(address=0x70)
+
+
+
 disp = Adafruit_SSD1306.SSD1306_128_32(rst=RST)
 
 # 128x64 display with hardware I2C:
@@ -47,57 +52,60 @@ disp = Adafruit_SSD1306.SSD1306_128_32(rst=RST)
 # disp = Adafruit_SSD1306.SSD1306_128_32(rst=RST, dc=DC, sclk=18, din=25, cs=22)
 
 # Initialize library.
-disp.begin()
+for i in range(8):
+    tca.writeRaw8(1<<(i+1))
+    
+    disp.begin()
 
-# Clear display.
-disp.clear()
-disp.display()
+    # Clear display.
+    disp.clear()
+    disp.display()
 
-# Create blank image for drawing.
-# Make sure to create image with mode '1' for 1-bit color.
-width = disp.width
-height = disp.height
-image = Image.new('1', (width, height))
+    # Create blank image for drawing.
+    # Make sure to create image with mode '1' for 1-bit color.
+    width = disp.width
+    height = disp.height
+    image = Image.new('1', (width, height))
 
-# Get drawing object to draw on image.
-draw = ImageDraw.Draw(image)
+    # Get drawing object to draw on image.
+    draw = ImageDraw.Draw(image)
 
-# Draw a black filled box to clear the image.
-draw.rectangle((0,0,width,height), outline=0, fill=0)
+    # Draw a black filled box to clear the image.
+    draw.rectangle((0,0,width,height), outline=0, fill=0)
 
-# Draw some shapes.
-# First define some constants to allow easy resizing of shapes.
-padding = 2
-shape_width = 20
-top = padding
-bottom = height-padding
-# Move left to right keeping track of the current x position for drawing shapes.
-x = padding
-# Draw an ellipse.
-draw.ellipse((x, top , x+shape_width, bottom), outline=255, fill=0)
-x += shape_width+padding
-# Draw a rectangle.
-draw.rectangle((x, top, x+shape_width, bottom), outline=255, fill=0)
-x += shape_width+padding
-# Draw a triangle.
-draw.polygon([(x, bottom), (x+shape_width/2, top), (x+shape_width, bottom)], outline=255, fill=0)
-x += shape_width+padding
-# Draw an X.
-draw.line((x, bottom, x+shape_width, top), fill=255)
-draw.line((x, top, x+shape_width, bottom), fill=255)
-x += shape_width+padding
+    # Draw some shapes.
+    # First define some constants to allow easy resizing of shapes.
+    padding = 2
+    shape_width = 20
+    top = padding
+    bottom = height-padding
+    # Move left to right keeping track of the current x position for drawing shapes.
+    x = padding
+    # Draw an ellipse.
+    draw.ellipse((x, top , x+shape_width, bottom), outline=255, fill=0)
+    x += shape_width+padding
+    # Draw a rectangle.
+    draw.rectangle((x, top, x+shape_width, bottom), outline=255, fill=0)
+    x += shape_width+padding
+    # Draw a triangle.
+    draw.polygon([(x, bottom), (x+shape_width/2, top), (x+shape_width, bottom)], outline=255, fill=0)
+    x += shape_width+padding
+    # Draw an X.
+    draw.line((x, bottom, x+shape_width, top), fill=255)
+    draw.line((x, top, x+shape_width, bottom), fill=255)
+    x += shape_width+padding
 
-# Load default font.
-font = ImageFont.load_default()
+    # Load default font.
+    font = ImageFont.load_default()
 
-# Alternatively load a TTF font.  Make sure the .ttf font file is in the same directory as the python script!
-# Some other nice fonts to try: http://www.dafont.com/bitmap.php
-#font = ImageFont.truetype('Minecraftia.ttf', 8)
+    # Alternatively load a TTF font.  Make sure the .ttf font file is in the same directory as the python script!
+    # Some other nice fonts to try: http://www.dafont.com/bitmap.php
+    #font = ImageFont.truetype('Minecraftia.ttf', 8)
 
-# Write two lines of text.
-draw.text((x, top),    'Hello',  font=font, fill=255)
-draw.text((x, top+20), 'World!', font=font, fill=255)
+    # Write two lines of text.
+    draw.text((x, top),    'Hello',  font=font, fill=255)
+    draw.text((x, top+20), 'World!', font=font, fill=255)
 
-# Display image.
-disp.image(image)
-disp.display()
+    # Display image.
+    disp.image(image)
+    disp.display()
